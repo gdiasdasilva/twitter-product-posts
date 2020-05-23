c249b521bb502d5555d3101aa7e17460cb8e78b8
Gonçalo Silva <gopdiasdasilva@gmail.com>
2020-05-23 13:26:19 +0100

    Add twitter log out route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root :to => 'home#index'
-  mount ShopifyApp::Engine, at: '/'
+  root to: "home#index"
+  mount ShopifyApp::Engine, at: "/"
 
   namespace :twitter do
-    get 'request_token'
-    get 'oauth_callback'
+    get "request_token"
+    get "oauth_callback"
+    get "log_out"
   end
 end
