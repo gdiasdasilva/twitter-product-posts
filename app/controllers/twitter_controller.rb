041cc87473f50ff1aac9e2837c103b669ac2f8de
Gonçalo Silva <gopdiasdasilva@gmail.com>
2020-05-23 13:31:25 +0100

    Add Twitter sign in and sign out

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,36 +2,7 @@
 
 # Handle twitter callbacks
 class TwitterController < AuthenticatedController
-  def oauth_callback
-    raise "different tokens!!!" if params[:oauth_token] != session[:token]
-
-    hash = {
-      oauth_token: session[:token],
-      oauth_token_secret: session[:token_secret]
-    }
-
-    request_token = OAuth::RequestToken.from_hash(oauth_consumer, hash)
-    access_token = request_token.get_access_token(oauth_verifier: params[:oauth_verifier])
-
-    # access token's oauth token and oauth secret should be persisted for future requests for this user
-    # access_token.token
-    # access_token.secret
-
-    oauth_params = { consumer: oauth_consumer, token: access_token }
-
-    uri = "https://api.twitter.com/1.1/users/show.json?screen_name="
-
-    hydra = Typhoeus::Hydra.new
-    req = Typhoeus::Request.new(uri, method: :get)
-
-    oauth_helper = OAuth::Client::Helper.new(req, oauth_params.merge(request_uri: uri))
-    req.options[:headers].merge!("Authorization": oauth_helper.header)
-
-    hydra.queue(req)
-    hydra.run
-
-    @response = req.response
-  end
+  skip_before_action :verify_authenticity_token
 
   def request_token
     url_encoded_callback_url = ERB::Util.url_encode(twitter_oauth_callback_url)
@@ -42,6 +13,30 @@ class TwitterController < AuthenticatedController
     session[:token_secret] = request_token.secret
 
     redirect_to request_token.authorize_url(oauth_callback: url_encoded_callback_url)
+  end
+
+  def oauth_callback
+    raise "Authentication error." unless params[:oauth_token] == session[:token]
+
+    hash = { oauth_token: session[:token], oauth_token_secret: session[:token_secret] }
+
+    request_token = OAuth::RequestToken.from_hash(oauth_consumer, hash)
+    access_token = request_token.get_access_token(oauth_verifier: params[:oauth_verifier])
+
+    twitter_account = TwitterAccount.find_or_create_by!(
+      twitter_id: access_token.params[:user_id],
+      twitter_handle: access_token.params[:screen_name],
+      token: access_token.token,
+      secret: access_token.secret
+    )
+
+    current_shop.update(twitter_account: twitter_account)
+    redirect_to root_path
+  end
+
+  def log_out
+    current_shop.update(twitter_account: nil)
+    redirect_to root_path
   end
 
   private
@@ -56,5 +51,9 @@ class TwitterController < AuthenticatedController
         debug_output: true
       )
     end
+  end
+
+  def current_shop
+    Shop.find_by!(shopify_domain: ShopifyAPI::Shop.current.domain)
   end
 end
