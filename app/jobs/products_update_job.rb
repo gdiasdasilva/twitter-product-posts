cb629f4e2b63179e2cab3a9dc8ee8c6ea14e9900
Gonçalo Silva <gopdiasdasilva@gmail.com>
2020-05-24 13:27:53 +0100

    Update ProductsUpdateJob to create message and call twitter service

--- app/jobs/products_update_job.rb
+++ app/jobs/products_update_job.rb
@@ -1,16 +1,44 @@
+# frozen_string_literal: true
+
 class ProductsUpdateJob < ActiveJob::Base
+  include ActionView::Helpers::NumberHelper
+
   def perform(shop_domain:, webhook:)
     shop = Shop.find_by(shopify_domain: shop_domain)
 
-    if shop.nil?
+    if shop.nil? || shop.twitter_account.nil?
       logger.error("#{self.class} failed: cannot find shop with domain '#{shop_domain}'")
       return
     end
 
-    product_handle = webhook[:handle]
-    product_url = "#{shop.shopify_domain}/products/#{product_handle}"
+    product_url       = "#{shop.shopify_domain}/products/#{webhook[:handle]}"
+    product_title     = webhook[:title]
+    product_min_price = webhook[:variants].map { |v| v[:price].to_f }.min
+    currency          = ""
 
     shop.with_shopify_session do
+      currency = ShopifyAPI::Shop.current.currency
+    end
+
+    return unless product_url.present? && product_title.present? && product_min_price.present?
+
+    formatted_price = format_currency(product_min_price, currency)
+
+    message = "Check out the most recent news about our #{product_title}.\n" \
+              "Available from #{formatted_price}." \
+              "\n\n" \
+              "#{product_url}"
+
+    PostToTwitterService.new(twitter_account: TwitterAccount.first, message: message).call
+  end
+
+  private
+
+  def format_currency(value, currency)
+    if currency == "USD"
+      number_to_currency(value, unit: "$", format: "%u%n")
+    else
+      number_to_currency(value, unit: "€", format: "%n%u")
     end
   end
 end
