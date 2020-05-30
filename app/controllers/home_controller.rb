33d11350a269461bdc194822e37384eef7f2234a
Gonçalo Silva <gopdiasdasilva@gmail.com>
2020-05-30 15:36:14 +0100

    Allow changing tweet template through a Enable button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@
 
 class HomeController < AuthenticatedController
   def index
+    @current_shop = current_shop
     @twitter_account = current_shop.twitter_account
     @active_tweet_template = current_shop.active_tweet_template
     @all_tweet_templates = TweetTemplate.all
