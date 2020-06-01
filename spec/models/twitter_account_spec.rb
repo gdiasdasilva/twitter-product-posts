9a182d07bbe365e3fb966073d7aec7e910f0b953
Gonçalo Silva <gopdiasdasilva@gmail.com>
2020-06-01 20:49:13 +0100

    Add spec to twitter account model

--- spec/models/twitter_account_spec.rb
+++ spec/models/twitter_account_spec.rb
@@ -1,5 +1,14 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe TwitterAccount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "does not allow repeated twitter_ids" do
+      create(:twitter_account, twitter_id: "abcdefgh")
+
+      twitter_account = build(:twitter_account, twitter_id: "abcdefgh")
+      expect(twitter_account).to be_invalid
+    end
+  end
 end
