6457ca1b095b0e38d1954966a5dcfbb02614ade7
Gonçalo Silva <gopdiasdasilva@gmail.com>
2020-06-01 20:49:05 +0100

    Add spec to tweet template model

--- spec/models/tweet_template_spec.rb
+++ spec/models/tweet_template_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe TweetTemplate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#example_tweet" do
+    let(:tweet_template) { create(:tweet_template, template: "[product-title] are my favourite") }
+
+    it "returns a dummy message with the current template" do
+      expect(tweet_template.example_tweet).to eq "Fortaleza Wooden Sunglasses are my favourite"
+    end
+  end
 end
