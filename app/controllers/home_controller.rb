eb7dd81b1b8a599023a2a79c9b932d904b101f88
Gonçalo Silva <gopdiasdasilva@gmail.com>
2020-06-05 01:43:53 +0100

    Rearrange homepage components

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,5 @@ class HomeController < AuthenticatedController
     @current_shop = current_shop
     @twitter_account = current_shop.twitter_account
     @active_tweet_template = current_shop.active_tweet_template
-    @all_tweet_templates = TweetTemplate.all
   end
 end
